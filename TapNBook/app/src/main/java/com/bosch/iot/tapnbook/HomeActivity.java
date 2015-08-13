777d40e67896c92b67765544ea35c4f4f6c4a99e
Tamilarasanonline <tamilarasanonline@gmail.com>
2015-08-14 06:11:26 +0800

    Calendar implementation

--- TapNBook/app/src/main/java/com/bosch/iot/tapnbook/HomeActivity.java
+++ TapNBook/app/src/main/java/com/bosch/iot/tapnbook/HomeActivity.java
@@ -1,5 +1,6 @@
 package com.bosch.iot.tapnbook;
 
+import android.accounts.Account;
 import android.app.Activity;
 import android.app.Dialog;
 import android.net.ConnectivityManager;
@@ -44,12 +45,71 @@ public class HomeActivity extends Activity {
     private Button btCancel;
     private TextView title;
     private Resources room;
+
+    final HttpTransport transport = AndroidHttp.newCompatibleTransport();
+    final JsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+    static final int REQUEST_ACCOUNT_PICKER = 1000;
+    static final int REQUEST_AUTHORIZATION = 1001;
+    static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
+    private static final String PREF_ACCOUNT_NAME = "tamilarasanonline@gmail.com";
+    private static final String[] SCOPES = {CalendarScopes.CALENDAR_READONLY};
+
+    GoogleAccountCredential credential;
+
+    com.google.api.services.calendar.Calendar mService;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         initializeWidgets();
+        initializeCalendar();
     }
+
+    private void initializeCalendar() {
+        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+        credential = GoogleAccountCredential.usingOAuth2(
+                getApplicationContext(), Arrays.asList(SCOPES))
+                .setBackOff(new ExponentialBackOff())
+                .setSelectedAccountName(settings.getString(PREF_ACCOUNT_NAME, null));
+
+        mService = new com.google.api.services.calendar.Calendar.Builder(
+                transport, jsonFactory, credential)
+                .setApplicationName("Google Calendar API Android Quickstart")
+                .build();
+        if (credential != null && credential.getAllAccounts() != null && credential.getSelectedAccount() == null) {
+            credential.setSelectedAccountName(PREF_ACCOUNT_NAME);
+            Toast.makeText(this, "GetSelectedAccount " + credential.getSelectedAccountName(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private boolean isGooglePlayServicesAvailable() {
+        final int connectionStatusCode =
+                GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if (GooglePlayServicesUtil.isUserRecoverableError(connectionStatusCode)) {
+            showGooglePlayServicesAvailabilityErrorDialog(connectionStatusCode);
+            return false;
+        } else if (connectionStatusCode != ConnectionResult.SUCCESS) {
+            return false;
+        }
+        return true;
+    }
+
+    void showGooglePlayServicesAvailabilityErrorDialog(
+            final int connectionStatusCode) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Dialog dialog = GooglePlayServicesUtil.getErrorDialog(
+                        connectionStatusCode,
+                        HomeActivity.this,
+                        REQUEST_GOOGLE_PLAY_SERVICES);
+                dialog.show();
+            }
+        });
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -66,11 +126,78 @@ public class HomeActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+    private void refreshResults() {
+        Toast.makeText(this, "refreshResults :: before Choose Account" + credential.getSelectedAccountName(), Toast.LENGTH_LONG).show();
+        if (credential.getSelectedAccountName() == null) {
+            chooseAccount();
+        } else {
+            if (isDeviceOnline()) {
+                new ApiAsyncTask(this).execute();
+            } else {
+                Toast.makeText(this, "No network connection available.", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    public void clearResultsText() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                title.setText("Retrieving data…");
+            }
+        });
+    }
+
+    public void updateResultsText(final List<String> dataStrings) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                if (dataStrings == null) {
+                    title.setText("Error retrieving data!");
+                } else if (dataStrings.size() == 0) {
+                    title.setText("No data found.");
+                } else {
+                    title.setText(TextUtils.join("Data retrieved using" +
+                            " the Google Calendar API:" + "\n\n", dataStrings));
+                }
+            }
+        });
+    }
+
+    public void updateStatus(final String message) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                title.setText(message);
+            }
+        });
+    }
+
+    private boolean isDeviceOnline() {
+        ConnectivityManager connMgr =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
+    private void chooseAccount() {
+        startActivityForResult(
+                credential.newChooseAccountIntent(), REQUEST_ACCOUNT_PICKER);
+    }
+
     @Override
     protected void onResume() {
         super.onResume();
         room = null;
         NdefMessage msgs[] = null;
+        if (isGooglePlayServicesAvailable()) {
+            Toast.makeText(this, "Google Service Available", Toast.LENGTH_LONG).show();
+            refreshResults();
+        } else {
+            Toast.makeText(this, "Google Play Services required: " +
+                    "after installing, close and relaunch this app.", Toast.LENGTH_LONG).show();
+        }
+
         if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
             Parcelable[] rawMsgs = getIntent().getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
             if (rawMsgs != null) {
@@ -109,11 +236,21 @@ public class HomeActivity extends Activity {
     }
 
     private void buttonOkClick(View v) {
-        if(room != null){
-            Toast.makeText(this, ""+room.toString() , Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(this, "No Meeting Room Selected yet :(", Toast.LENGTH_LONG).show();
-        }
+//        SharedPreferences settings = getPreferences(Context.MODE_PRIVATE);
+
+//        credential = GoogleAccountCredential.usingOAuth2(
+//                getApplicationContext(), Arrays.asList(SCOPES))
+//                .setBackOff(new ExponentialBackOff())
+//                .setSelectedAccountName(settings.getString(PREF_ACCOUNT_NAME, null));
+        Toast.makeText(this, "" + credential, Toast.LENGTH_LONG).show();
+
+//        if (room != null) {
+//            Toast.makeText(this, "" + room.toString(), Toast.LENGTH_LONG).show();
+//
+//
+//        } else {
+//            Toast.makeText(this, "No Meeting Room Selected yet :(", Toast.LENGTH_LONG).show();
+//        }
     }
 
     private void buttonCancelClick(View v) {
@@ -134,9 +271,7 @@ public class HomeActivity extends Activity {
     }
 
     private Resources getResourceName(String payload) {
-        Toast.makeText(this, payload, Toast.LENGTH_LONG).show();
         if (payload.trim().startsWith("en")) {
-            Toast.makeText(this, "inSIDE iF", Toast.LENGTH_LONG).show();
             String strRoom = payload.replaceFirst("en", "");
             for (Resources room : Resources.values()) {
                 if (room.toString().trim().equalsIgnoreCase(strRoom.trim())) {
